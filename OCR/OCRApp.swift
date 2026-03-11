ee1bac3ece09a0f28c07fadfc47743df3a0f342a
Jamie Chen <jamie950315@gmail.com>
2026-03-11 10:37:13 +0800

    Initial Commit

---
+++ OCR/OCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCRApp.swift
+//  OCR
+//
+//  Created by jamie chen on 2026/3/11.
+//
+
+import SwiftUI
+
+@main
+struct OCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OCRTests/OCRTests.swift
@@ -0,0 +1,17 @@
+//
+//  OCRTests.swift
+//  OCRTests
+//
+//  Created by jamie chen on 2026/3/11.
+//
+
+import Testing
+@testable import OCR
+
+struct OCRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
